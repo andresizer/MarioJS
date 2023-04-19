82ec1cad6617b6c25a0bd4842855e4b4ab45cf7f
André Sizer <andresizer@gmail.com>
2023-04-19 15:30:34 -0300

    Mario JS Game
    Jogo desenvolvido em CSS e JS.

---
+++ js/script.js
@@ -0,0 +1,58 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const gameover = document.querySelector('.gameover');
+
+
+const audioGameOver = new Audio();
+    audioGameOver.src = './sound/gameover.mp3';
+    audioGameOver.loop = false;
+
+
+const jump = () => {
+    mario.classList.add('jump');
+
+ 
+    //   audioElement.play()
+
+
+
+    setTimeout(() => {
+        
+        mario.classList.remove('jump');
+
+    }, 500);
+
+}
+
+
+const loop = setInterval(() => {
+    
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+   
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 90) {
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`
+
+        // mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`
+
+        mario.src = './img/gameover.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+        mario.classList.add('mario-gameover');
+
+       audioGameOver.playedOnce()
+      
+
+        clearInterval();
+       
+    }
+
+}, 10);
+
+
+document.addEventListener('click', jump);
+
+    
