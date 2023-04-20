768e04220592d21b2ec9827dd27232436fc5cbfe
André Sizer <andresizer@gmail.com>
2023-04-20 17:36:11 -0300

    muitas mudanças
    reinicio do jogo e contador de tempo em configuração

--- js/script.js
+++ js/script.js
@@ -1,20 +1,47 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 const gameover = document.querySelector('.gameover');
+const restart = document.querySelector('.restart');
 
 
 const audioGameOver = new Audio();
     audioGameOver.src = './sound/gameover.mp3';
-    audioGameOver.loop = false;
+    
 
+// Define uma variável para contar o tempo
+let tempo = 0;
+
+// Cria um elemento para exibir o tempo decorrido
+let contador = document.createElement("score");
+contador.classList.add('score');
+document.body.appendChild(contador);
+
+// Define a função que será executada a cada segundo
+function atualizarTempo() {
+  tempo++; // Incrementa o tempo em 1 segundo
+  contador.innerHTML = tempo; // Atualiza o texto do elemento
+}
+
+// Define o intervalo de tempo em que a função será executada (1000ms = 1 segundo)
+let intervalo = setInterval(atualizarTempo, 100);
+
+function pararContador() {
+    clearInterval(intervalo); // Para a execução do intervalo de tempo
+  }
+
+ // Cria o elemento hiscore
+let HighScore = document.createElement("hscore");
+HighScore.classList.add('hscore');
+document.body.appendChild(HighScore); 
+HighScore.innerHTML = 0;
+
+
+
+
+// Configuração do pulo 
 
 const jump = () => {
     mario.classList.add('jump');
-
- 
-    //   audioElement.play()
-
-
 
     setTimeout(() => {
         
@@ -24,43 +51,82 @@ const jump = () => {
 
 }
 
+function reiniciarJogo() {
+    // Reiniciar a contagem, imagem de game over e reset saírem da tela, mario voltar a caminhar e cano voltar a animação
+   
+    // Cano
+    // Estilo left é resetado, a classe pipe-parado é removida e a classe pipe é adicionada novamente
+    pipe.style.left = '';
+    pipe.classList.remove('pipe-parado');   
+    pipe.classList.add('pipe');
+
+    // Game over e reset
+    gameover.src = '';
+    restart.src = ''; 
+
+    // Mario
+    mario.src = './img/mario-walking.gif';
+    mario.style.width = '';
+    mario.style.marginLeft = '';
+    mario.style.bottom = 0;
+    mario.classList.remove('mario-gameover');
+
+    // Contagem
+
+    //    HighScore.innerHTML = tempo;
+    // contador.innerHTML = tempo; 
+     
+
+
+  }
+
+// Definições para o fim do jogo 
 
 const fimDoJogo = setInterval(() => {
-    
+ 
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
    
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 90) {
 
-        pipe.style.animation = 'none';
-        pipe.style.left = `${pipePosition}px`
-
-        // mario.style.animation = 'none';
-        mario.style.bottom = `${marioPosition}px`
-
+        // Classe pipe removida e adicionada a classe pipe-parado, sem animação 
+        pipe.classList.remove('pipe');
+        pipe.classList.add('pipe-parado'); 
+        // A esquerda é a posição do cano na hora do encontro com o mario
+        pipe.style.left = `${pipePosition}px`;
+       
+       
+        mario.style.bottom = `${marioPosition}px`;
         mario.src = './img/gameover.png';
         mario.style.width = '75px';
         mario.style.marginLeft = '50px';
         mario.classList.add('mario-gameover');
 
-       audioGameOver.play()
+        audioGameOver.play()
+        audioGameOver.loop = false;
 
-       gameover.src = './img/game_over.png';
-    
-
-      
-
-        clearInterval();
+        gameover.src = './img/game_over.png';
+        restart.src = './img/restar.jpg';
        
+        pararContador();
+        clearInterval();
+        // Criando a Highscore 
+        if (tempo > HighScore.innerHTML) {
+            HighScore.innerHTML = tempo;
+        }
+       
+        document.addEventListener('keydown', function() {
+            reiniciarJogo();
+        });
+        
+
+
     }
 
 }, 10);
 
 
+
+HighScore.innerHTML = "";
+
 document.addEventListener('click', jump);
-
-const restart = () => {
-    
-}
-
-    
